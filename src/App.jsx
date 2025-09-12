b6aafad6116d2cfc766b3800db76e680327156c6
john0210 <john0210@hanmail.net>
2025-09-12 11:13:30 +0900

    250912 라우터 셋팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from './components/common/Header.jsx';
 import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header></Header>
+      <main>
+        <Outlet />
+      </main>
     </>
   )
 }
